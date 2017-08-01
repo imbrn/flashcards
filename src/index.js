727be59918dc4e31a5c4d24a6645f746d52f920b
Bruno C. Couto <brunoccouto@gmail.com>
2017-08-01 18:01:14 -0300

    Removed fake decks.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './app';
-import DecksActions from './data/decks-actions';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-// TODO: remover testes (também: import DecksActions)
-DecksActions.addDeck("Pronouns", "Pronomes");
-DecksActions.addDeck("Nouns", "Substantivos");
-window.DecksActions = DecksActions;
