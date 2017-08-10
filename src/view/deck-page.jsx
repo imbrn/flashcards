b745f9a9ee5efb7ad8bd0fbfe59a6791e161d9ad
Bruno C. Couto <brunoccouto@gmail.com>
2017-08-10 18:29:14 -0300

    Improved deck page.

--- src/view/deck-page.jsx
+++ src/view/deck-page.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Container } from 'flux/utils';
+import { createStyleSheet, withStyles } from 'material-ui/styles';
 import DecksActions from '../data/decks-actions';
 import DeckStore from '../data/deck-store.js';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import Actions from './actions';
 import ActionsTypes from './actions-types';
+
+/*
+DeckPage stylesheets.
+*/
+const stylesheet = createStyleSheet('DeckPage', () => {
+  return {
+    root: {
+      height: '100%'
+    },
+    noCardsContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  };
+});
 
 /**
  * One deck page.
@@ -39,18 +58,39 @@ class DeckPage extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        {this._renderPage(classes)}
+      </div>
+    );
+  }
+
+  _renderPage(classes) {
     if (this.state.deck) {
-      return this._renderDeck();
+      return this._renderCards(classes);
     } else {
       return <span>No deck</span>;
     }
   }
 
-  _renderDeck() {
+  _renderCards(classes) {
+    if (this.state.deck.cards.size > 0) {
+      return this._renderCardsItems(classes);
+    } else {
+      return this._renderNoCardsMessage(classes);
+    }
+  }
+
+  _renderCardsItems() {
+    return <span/>;
+  }
+
+  _renderNoCardsMessage(classes) {
     return (
-      <div>
-        <Typography>{this.state.deck.name}</Typography>
-        {this._renderAddCard()}
+      <div className={classes.noCardsContainer}>
+        <Typography type="display1">No cards</Typography>
+        <Typography type="subheading">There's no added cards yet.</Typography>
       </div>
     );
   }
@@ -80,7 +120,7 @@ class PageTitle extends React.Component {
     if (this.state.deck) {
       return this._renderDeck();
     } else {
-      return <span/>;
+      return <span />;
     }
   }
 
@@ -100,7 +140,7 @@ const TitleContainer = Container.create(PageTitle);
 DeckPageActions
 */
 class PageActions extends React.Component {
-  
+
   render() {
     return (
       <Button color="inherit" onClick={this.handleAddCardClick.bind(this)}>
@@ -108,14 +148,15 @@ class PageActions extends React.Component {
       </Button>
     );
   }
-  
+
   handleAddCardClick() {
     Actions.execute(ActionsTypes.ADD_CARD);
   }
 
 }
 
-export default Container.create(DeckPage);
+export default withStyles(stylesheet)(Container.create(DeckPage));
+
 export {
   TitleContainer as DeckPageTitle,
   PageActions as DeckPageActions
