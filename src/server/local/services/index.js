b03bd324c3144535518c4513712bc5f07706e3b4
Bruno C. Couto <brunoccouto@gmail.com>
2017-09-16 17:26:04 -0300

    Fixes imports of services index.js

--- src/server/local/services/index.js
+++ src/server/local/services/index.js
@@ -1,9 +1,11 @@
 import AddDeck from './AddDeck';
 import AddCard from './AddCard';
 import DeleteCard from './DeleteCard';
+import UpdateCard from './UpdateCard';
 
 export {
   AddDeck,
   AddCard,
-  DeleteCard
+  DeleteCard,
+  UpdateCard
 };
