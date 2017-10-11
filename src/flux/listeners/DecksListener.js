c2ff2d6bca806dc76f61bdbb6053766f3f3d9ca7
Bruno C. Couto <brunoccouto@gmail.com>
2017-10-11 15:49:14 -0300

    Joined DecksListener and CardsListeners

--- src/flux/listeners/DecksListener.js
+++ src/flux/listeners/DecksListener.js
@@ -4,6 +4,10 @@ import Types from '../actions/Types';
 
 class DecksListener {
 
+  constructor() {
+    this.removeCardsSnapshotsListeners = {};
+  }
+
   start() {
     this.removeAuthListener = auth().onAuthStateChanged(this.authStateChanged.bind(this));
   }
@@ -39,6 +43,45 @@ class DecksListener {
       id: doc.id,
       data: doc.data()
     });
+    this.startListeningCards(doc);
+  }
+
+  startListeningCards(doc) {
+    const listener = doc.ref.collection('cards').onSnapshot(snapshot => {
+      snapshot.docChanges.forEach(change => {
+        if (change.type === 'added') this.cardAdded(doc, change.doc);
+        if (change.type === 'modified') this.cardModified(doc, change.doc);
+        if (change.type === 'removed') this.cardRemoved(doc, change.doc);
+      });
+    });
+    this.removeCardsSnapshotsListeners[doc.id] = listener;
+  }
+
+  cardAdded(deckDoc, cardDoc) {
+    Dispatcher.dispatch({
+      type: Types.ON_CARD_ADDED,
+      deckId: deckDoc.id,
+      id: cardDoc.id,
+      data: cardDoc.data(),
+    });
+  }
+
+  cardModified(deckDoc, cardDoc) {
+    Dispatcher.dispatch({
+      type: Types.ON_CARD_CHANGED,
+      deckId: deckDoc.id,
+      id: cardDoc.id,
+      data: cardDoc.data()
+    });
+  }
+
+  cardRemoved(deckDoc, cardDoc) {
+    Dispatcher.dispatch({
+      type: Types.ON_CARD_REMOVED,
+      deckId: deckDoc.id,
+      id: cardDoc.id,
+      data: cardDoc.data()
+    });
   }
 
   deckModified(doc) {
@@ -50,6 +93,7 @@ class DecksListener {
   }
 
   deckRemoved(doc) {
+    this.stopListeningCards(doc);
     Dispatcher.dispatch({
       type: Types.ON_DECK_REMOVED,
       id: doc.id,
@@ -57,11 +101,25 @@ class DecksListener {
     });
   }
 
+  stopListeningCards(doc) {
+    this.removeCardsSnapshotsListeners[doc.id]();
+    delete this.removeCardsSnapshotsListeners[doc.id];
+  }
+
   onSignOut() {
+    this.removeAllCardsSnapshotsListeners();
     this.removeDecksListener();
   }
 
+  removeAllCardsSnapshotsListeners() {
+    for (const key in this.removeCardsSnapshotsListeners) {
+      this.removeCardsSnapshotsListeners[key]();
+    }
+    this.removeCardsSnapshotsListeners = {};
+  }
+
   stop() {
+    this.removeAllCardsSnapshotsListeners();
     this.removeDecksListener();
     this.removeAuthListener();
   }
--- src/flux/listeners/initialize.js
+++ src/flux/listeners/initialize.js
@@ -1,11 +1,9 @@
 import AuthListener from './AuthListener';
 import DecksListener from './DecksListener';
-import CardsListener from './CardsListener';
 
 const listeners = [
   new AuthListener(),
   new DecksListener(),
-  new CardsListener()
 ];
 
 function initialize() {
