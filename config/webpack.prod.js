3ba8aa37f2e21f16ebcf14f8d78629a227a37454
Bruno C. Couto <brunodev02221@gmail.com>
2017-11-08 12:43:08 -0200

    Update build process to uglify when in production
    This is applied only for the bundle js file.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,9 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 });
