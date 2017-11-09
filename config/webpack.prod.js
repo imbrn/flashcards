c7de6f835030f46ce634c1898fa8a9c6c5eaa915
Bruno C. Couto <brunodev02221@gmail.com>
2017-11-09 15:00:36 -0200

    Update build process to clean before building
    The build folder will be removed whenever the build process is goint to
    execute.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const paths = require('./paths.js');
 
 module.exports = merge(common, {
   plugins: [
+    new CleanWebpackPlugin([ paths.build ], {
+      root: paths.root
+    }),
     new UglifyJsPlugin()
   ]
 });
