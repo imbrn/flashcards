115542eb3be516b836a0b3b95ffbc4a5dcce0572
Bruno C. Couto <brunodev02221@gmail.com>
2017-11-17 13:42:02 -0200

    Update linter config to allow class properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,40 @@
 module.exports = {
-  "extends": "airbnb",
   "env": {
-    "browser": true
+    "browser": true,
+    "es6": true,
+    "node": true,
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "comma-dangle": [
+      "error",
+      "only-multiline"
+    ],
   }
 };
