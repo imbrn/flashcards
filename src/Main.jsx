13888710afcec55b45fccfb1f8a6e6ec0f1ad7dc
Bruno C. Couto <brunodev02221@gmail.com>
2017-11-30 19:54:44 -0200

    Fix router Link bug
    When accessing a page from a React Router Link it wasn't being
    rendered, and this is because of the react-redux pure component
    management. A call to "withRouter" was needed to fix this problem.

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import "firebase/firestore";
 import { auth, firestore, initializeApp } from "firebase";
@@ -138,4 +139,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Main);
+export default withRouter(connect(mapStateToProps)(Main));
