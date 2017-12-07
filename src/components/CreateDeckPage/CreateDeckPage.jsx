cb29188a72b60b2a1a4c561766165a10ef112f11
Bruno C. Couto <brunodev02221@gmail.com>
2017-12-07 14:47:25 -0200

    Add TextField component

--- src/components/CreateDeckPage/CreateDeckPage.jsx
+++ src/components/CreateDeckPage/CreateDeckPage.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import styles from "./CreateDeckPage.css";
 import Navbar from "../Navbar";
+import TextField from "../TextField";
 
 const CreateDeckPage = () => {
   return (
     <div>
       <Navbar title="New deck" />
-      Create deck
+      <div className={styles.content}>
+        <div>
+          <TextField placeholder="Example" type="primary" size="big" />
+        </div>
+        <div>
+          <TextField placeholder="Example" type="danger" size="small" />
+        </div>
+        <div>
+          <TextField placeholder="Example" />
+        </div>
+        <div>
+          <TextField placeholder="Example" />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/TextField/TextField.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PropTypesUtils from "../prop-types-utils";
+import classnames from "classnames";
+import styles from "./TextField.css";
+
+const TextField = ({
+  className,
+  type = "normal",
+  size = "medium",
+  ...rest
+}) => {
+  const classNames = classnames(
+    styles.textField,
+    styles[type],
+    styles[size],
+    className
+  );
+
+  return <input {...rest} type="text" className={classNames} />;
+};
+
+TextField.propTypes = {
+  className: PropTypesUtils.className,
+  size: PropTypes.oneOf(["small", "medium", "big"]),
+  type: PropTypes.oneOf([
+    "normal",
+    "primary",
+    "secondary",
+    "tertiary",
+    "danger",
+    "success",
+    "info"
+  ])
+};
+
+export default TextField;
