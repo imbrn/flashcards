fe62402bec8b7af115a7bc261345a0f23ef4ee96
Bruno C. Couto <brunodev02221@gmail.com>
2017-12-18 13:40:10 -0200

    Add colors support to Icon component

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import PropTypesUtils from "../prop-types-utils.js";
 import classnames from "classnames";
+import styles from "./Icon.css";
 
-const Icon = ({ icon, className, ...rest }) => {
+const Icon = ({ icon, color="normal", className, ...rest }) => {
+  const modifiersClasses = classnames(
+    styles[color]
+  );
   return (
-    <span {...rest} className={classnames(className)}>
+    <span {...rest} className={classnames(styles.icon, modifiersClasses, className)}>
       <i className={icon} />
     </span>
   );
@@ -13,6 +17,7 @@ const Icon = ({ icon, className, ...rest }) => {
 
 Icon.propTypes = {
   icon: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(["normal", "primary", "secondary", "tertiary", "info"]),
   className: PropTypesUtils.className
 };
 
