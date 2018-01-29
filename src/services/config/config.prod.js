f97c0e74a33030a4df601fc1ad294b333b3985c0
Bruno C. Couto <brunodev02221@gmail.com>
2018-01-29 11:53:24 -0200

    Update configurations

--- src/services/config/config.prod.js
+++ src/services/config/config.prod.js
@@ -1,10 +1,5 @@
 const config = {
-  apiKey: "AIzaSyC5JDUSuIXqa01004JDgbk4jbhMCTODczo",
-  authDomain: "flashcards-f39c0.firebaseapp.com",
-  databaseURL: "https://flashcards-f39c0.firebaseio.com",
-  projectId: "flashcards-f39c0",
-  storageBucket: "flashcards-f39c0.appspot.com",
-  messagingSenderId: "66998935010"
+  // Add production firebase configuration
 };
 
 module.exports = config;
